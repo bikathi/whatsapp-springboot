cbeb05fe93277bff800d7b7d423428fdd38eb29e
bikathi-martin <bikathimartin@outlook.com>
2025-02-24 11:58:59 +0300

    Remove webhook-related endpoints

--- src/main/java/npc/bikathi/whatsappintg/controller/WhatsAppMessagingController.java
+++ src/main/java/npc/bikathi/whatsappintg/controller/WhatsAppMessagingController.java
@@ -64,39 +64,4 @@ public class WhatsAppMessagingController {
             return ResponseEntity.status(500).body(e.getMessage());
         }
     }
-
-    @PostMapping(value = "/webhook")
-    public ResponseEntity<String> callbackWebhook(@RequestBody UserResponse userResponse) {
-        log.info("Message received from user is: {}", userResponse.getResponse());
-        try {
-            if(!userResponse.getResponse().isBlank() && userResponse.getResponse().contains("from")) {
-                ResponseEntity.status(HttpStatus.OK).body(userResponse.getResponse());
-            } else {
-                return ResponseEntity.noContent().build();
-            }
-        } catch (Exception e) {
-            log.error("Failed to understand callback data! Cause: {}", e.getMessage());
-            return ResponseEntity.status(500).body(e.getMessage());
-        }
-
-        return null;
-    }
-
-    @GetMapping("/webhook")
-    public ResponseEntity<String> verifyWebhook(
-        @RequestParam(name = "hub.mode", required = false) String mode,
-        @RequestParam(name = "hub.verify_token", required = false) String token,
-        @RequestParam(name = "hub.challenge", required = false) String challenge,
-        HttpServletRequest request
-    ) {
-        log.info("Webhook triggered! mode: {}, token: {}, challenge: {}", mode, token, challenge);
-        log.info("Request url: {}", request.getRequestURL().toString());
-        final String WEBHOOK_VERIFY_TOKEN = "ELDSRYQG9QXM15XBK9N7";
-        if ("subscribe".equals(mode) && WEBHOOK_VERIFY_TOKEN.equals(token)) {
-            System.out.println("Webhook verified successfully!");
-            return ResponseEntity.ok(challenge);
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-    }
 }
