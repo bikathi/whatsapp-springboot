d4ffee3232cc77bc7a8ab9488559ea6dbdfdf274
bikathi-martin <bikathimartin@outlook.com>
2025-03-02 12:41:26 +0300

    Create the root controller for `/` endpoint

---
+++ src/main/java/npc/bikathi/whatsappintg/controller/RootController.java
@@ -0,0 +1,13 @@
+package npc.bikathi.whatsappintg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String rootEndpoint() {
+        return "Welcome to the root endpoint!";
+    }
+}
