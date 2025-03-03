a3082984fca6fce4715ff19db86b218d772f1563
bikathi-martin <bikathimartin@outlook.com>
2025-03-03 21:24:08 +0300

    Add prop for managing the callback challenge verification token

--- src/main/java/npc/bikathi/whatsappintg/config/PropertiesConfig.java
+++ src/main/java/npc/bikathi/whatsappintg/config/PropertiesConfig.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 @AllArgsConstructor
 @NoArgsConstructor
+@Scope(value = "singleton")
 @ConfigurationProperties(prefix = "api.whatsapp")
 public class PropertiesConfig {
     private String apiKey;
     private String senderPhoneId;
+    private String callbackValidationToken;
 }
