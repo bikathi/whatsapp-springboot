6d0f6fad52960d5ae7ff95da6ae3d4d2415302cc
bikathi-martin <bikathimartin@outlook.com>
2025-03-05 22:11:14 +0300

    Add simple log for ensuring response validation is being achieved successfully

--- src/main/java/npc/bikathi/whatsappintg/util/CommunicationsUtil.java
+++ src/main/java/npc/bikathi/whatsappintg/util/CommunicationsUtil.java
@@ -1,13 +1,16 @@
 package npc.bikathi.whatsappintg.util;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CommunicationsUtil {
     @Async(value = "asyncTaskExecutor")
     public final void returnResponse(@NotNull String externPartId, @NotNull String message) {
         // TODO: implement some REST call here
+        log.info("Congrats! Message to be sent to Part Sultan! ExternPartId: {}, Message: {}", externPartId, message);
     }
 }
